096a44ddbf44b01bf48d7d09ecf1d44f72fc9045
nayodahl <anthony.fachaux@gmail.com>
2020-12-16 18:56:10 +0100

    added easyAdminBundle, included compiled bootstrap JS

--- assets/app.js
+++ assets/app.js
@@ -10,4 +10,6 @@ import './styles/app.css';
 import './styles/app.less';
 import './styles/app.scss';
 
+require('bootstrap');
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Controller\PokeController;
+use App\Entity\Game;
+use App\Entity\Pokemon;
+use App\Entity\Tournament;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="app_admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Poketournament');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return [
+            MenuItem::linktoDashboard('Dashboard', 'fa fa-home'),
+
+            MenuItem::section('Pokemons'),
+            MenuItem::linkToCrud('Pokemons', 'fa fa-tags', Pokemon::class),
+
+            MenuItem::section('Tournaments'),
+            MenuItem::linkToCrud('Games', 'fa fa-tags', Game::class)
+            ->setDefaultSort(['createdAt' => 'DESC']),
+            MenuItem::linkToCrud('Tournaments', 'fa fa-tags', Tournament::class)
+            ->setDefaultSort(['date' => 'DESC']),
+
+        ];
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
