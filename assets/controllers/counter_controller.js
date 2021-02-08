a84f9a6ef01e82c64861f95ec35c326be0970dfd
nayodahl <anthony.fachaux@gmail.com>
2021-02-08 21:40:03 +0100

    js counter working on click

--- assets/controllers/counter_controller.js
+++ assets/controllers/counter_controller.js
@@ -3,8 +3,12 @@ import { Controller } from 'stimulus';
 export default class extends Controller {
     connect() {
         this.count = 0;
-        this.element.addEventListener(), click;
-        this.count++;
-        this.element.innerHTML = this.count;
+
+        var hello = () => {
+            this.count++;
+            this.element.innerHTML = this.count;
+        }
+
+        this.element.addEventListener("click", hello);
     }
 }
