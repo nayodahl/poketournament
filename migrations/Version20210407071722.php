966a6fc66c9d63e07182be0afdb6c8b45a63db3e
nayodahl <anthony.fachaux@gmail.com>
2021-04-07 12:28:55 +0200

    CI correction on a doctrine migration

--- migrations/Version20210407071722.php
+++ migrations/Version20210407071722.php
@@ -20,7 +20,6 @@ final class Version20210407071722 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE types_pokemons');
         $this->addSql('ALTER TABLE pokemon ADD type1_id INT DEFAULT NULL, ADD type2_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE pokemon ADD CONSTRAINT FK_62DC90F3BFAFA3E1 FOREIGN KEY (type1_id) REFERENCES type (id)');
         $this->addSql('ALTER TABLE pokemon ADD CONSTRAINT FK_62DC90F3AD1A0C0F FOREIGN KEY (type2_id) REFERENCES type (id)');
@@ -31,9 +30,6 @@ final class Version20210407071722 extends AbstractMigration
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE types_pokemons (type_id INT NOT NULL, pokemon_id INT NOT NULL, INDEX IDX_F0BF57132FE71C3E (pokemon_id), INDEX IDX_F0BF5713C54C8C93 (type_id), PRIMARY KEY(type_id, pokemon_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE types_pokemons ADD CONSTRAINT FK_F0BF57132FE71C3E FOREIGN KEY (pokemon_id) REFERENCES pokemon (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE types_pokemons ADD CONSTRAINT FK_F0BF5713C54C8C93 FOREIGN KEY (type_id) REFERENCES type (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE pokemon DROP FOREIGN KEY FK_62DC90F3BFAFA3E1');
         $this->addSql('ALTER TABLE pokemon DROP FOREIGN KEY FK_62DC90F3AD1A0C0F');
         $this->addSql('DROP INDEX IDX_62DC90F3BFAFA3E1 ON pokemon');
