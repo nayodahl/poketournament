297f7d6a4bc9a4a0497e21b127cd8f16913a12ff
nayodahl <anthony.fachaux@gmail.com>
2021-04-10 17:58:52 +0200

    restyling still in progress

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('cyan', './assets/cyan.js')
+    .addEntry('cyan', './assets/color-blue.js')
     //.addEntry('pokedex', './assets/pokedex.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
