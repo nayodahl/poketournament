b83a31bf82ec815b5ae92f5d3dc07455b1fdc2c7
Anthony <anthony.fachaux@tld-europe.com>
2022-12-17 22:02:36 +0100

    simplify slugger command

--- app/src/Command/PokemonRefreshSlugCommand.php
+++ app/src/Command/PokemonRefreshSlugCommand.php
@@ -15,7 +15,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class PokemonRefreshSlugCommand extends Command
 {
     protected static $defaultName = 'app:pokemon:refreshSlug';
-    
+
     public function __construct(
         private PokemonRepository $pokemonRepo,
         private EntityManagerInterface $entityManager,
@@ -23,13 +23,10 @@ class PokemonRefreshSlugCommand extends Command
     ) {
         parent::__construct();
     }
-    
+
     protected function configure(): void
     {
-        $this
-            ->setDescription('refresh slug property of Pokemons and store them')
-            ->addOption('dry-run', null, InputOption::VALUE_NONE, 'Dry run')
-        ;
+        $this->setDescription('refresh slug property of Pokemons and store them');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -40,39 +37,20 @@ class PokemonRefreshSlugCommand extends Command
         $numberOfPokemon = count($pokemons);
         $numberOfUpdate = 0;
 
-        if ($input->getOption('dry-run')) {
-            $io->note('Dry run mode enabled');
-            
-            // creates a new progress bar (units = total number of pokemons fetched)
-            $progressBar = new ProgressBar($output, $numberOfPokemon);
-           
-            foreach ($pokemons as $pokemon) {
-                $previousSlug = $pokemon->getSlug();
-                $newSlug = $this->slugger->slugIt($pokemon->getName());
-                
-                if ($newSlug !== $previousSlug) {
-                    $numberOfUpdate++;
-                }
-                $progressBar->advance();
-            };
-            $progressBar->finish();
-        } else {
-            // creates a new progress bar (units = total number of pokemons fetched)
-            $progressBar = new ProgressBar($output, $numberOfPokemon);
-           
-            foreach ($pokemons as $pokemon) {
-                $previousSlug = $pokemon->getSlug();
-                $newSlug = $this->slugger->slugIt($pokemon->getName());
+        $progressBar = new ProgressBar($output, $numberOfPokemon);
 
-                if ($newSlug !== $previousSlug) {
-                    $pokemon->setSlug($newSlug);
-                    $this->entityManager->flush();
-                    $numberOfUpdate++;
-                }
-                $progressBar->advance();
-            };
-            $progressBar->finish();
+        foreach ($pokemons as $pokemon) {
+            $previousSlug = $pokemon->getSlug();
+            $newSlug = $this->slugger->slugIt($pokemon->getName());
+
+            if ($newSlug !== $previousSlug) {
+                $pokemon->setSlug($newSlug);
+                $this->entityManager->flush();
+                $numberOfUpdate++;
+            }
+            $progressBar->advance();
         }
+        $progressBar->finish();
 
         $io->success(sprintf('Updated %d slugs from %d pokemons.', $numberOfUpdate, $numberOfPokemon));
 
